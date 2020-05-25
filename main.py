cd7e17b4e85b72453d657d584f3e0b5898d7a03c
GodSaveTheDoge <51802433+GodSaveTheDoge@users.noreply.github.com>
2020-05-25 22:58:38 +0200

    v 2.0 - Rewrote everything

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+if sys.version_info.minor < 7:
+    print("You should use python 3.7 or higher.\n")
+
+from pyrogram import Client
+
+bot = Client(
+    "MultiUserbot",
+    config_file="config.ini")
+
+
+bot.start()
