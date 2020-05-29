dc7c8262cb2168a7ae0650e5f1c71cd9fbd78e85
HtmlLoverExe <60547692+HtmlLoverExe@users.noreply.github.com>
2020-05-29 12:14:41 +0200

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,1 +1,8 @@
+import configparser
+import time
 
+from pyrogram import Client, Filters
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+prefixes = list(config["prefixes"].keys())
