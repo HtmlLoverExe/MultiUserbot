64993d36ad77dea6d8d3edb4c1a33f8c80316601
HtmlLoverExe <60547692+HtmlLoverExe@users.noreply.github.com>
2020-05-29 12:27:24 +0200

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -6,3 +6,13 @@ from pyrogram import Client, Filters
 config = configparser.ConfigParser()
 config.read("config.ini")
 prefixes = list(config["prefixes"].keys())
+
+@Client.on_message(Filters.user("self") & Filters.command("test", prefixes=prefixes))
+def test_command(c, msg):
+ import random
+numcasuale=str(random.randint(0,100))
+print("You are " + numcasuale + " %gay!")
+
+
+
+print("[MultiUserbot] Loaded \"test.py\" plugin")
