31a7e0f95c171bba15b139e7dde056b07604bb66
HtmlLoverExe <60547692+HtmlLoverExe@users.noreply.github.com>
2020-05-29 20:56:44 +0200

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -10,9 +10,9 @@ prefixes = list(config["prefixes"].keys())
 
 @Client.on_message(Filters.user("self") & Filters.command("test", prefixes=prefixes))
 def test_command(c, msg):
- import random
-    numcasuale=str(random.randint(0,100))
-     msg.edit_text("You are " + numcasuale + " %gay!")
+  import random
+     numcasuale=str(random.randint(0,100))
+        msg.edit_text("You are " + numcasuale + " %gay!")
 
 
 
